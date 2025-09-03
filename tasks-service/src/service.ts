03c77360d7b422823716998b3b396332cfb8cfc3
Victor Mazo <viamazoba@unal.edu.co>
2025-09-02 23:00:29 -0500

    feat: Se disponibilizan los enrutamientos del task-service

--- tasks-service/src/service.ts
+++ tasks-service/src/service.ts
@@ -3,13 +3,9 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import morgan from 'morgan'
 import { corsConfig } from './config/cors'
-import { healthchecktHandler } from './controllers/healthcheck.controller'
+import { healthChecktHandler } from './controllers/Healthcheck.controller'
 import { connetDB } from './config/db'
-/* import examRoutes from './routes/exam.routes'
-import schoolRoutes from './routes/school.routes'
-import userRoutes from './routes/user.routes'
-import groupRoutes from './routes/group.routes' */
-//import authRoutes from './routes/authRoutes'
+import taskRoutes from './routes/task.routes'
 
 
 dotenv.config()
@@ -17,20 +13,12 @@ connetDB()
 const app = express()
 app.use(cors(corsConfig))
 
-// Logging
 app.use(morgan('dev'))
 
-// Leer datos de formulario
 app.use(express.json())
 
-//Routes
-//app.use('/api/auth', authRoutes)
-/* app.use('/api/v1/exams', examRoutes)
-app.use('/api/v1/schools', schoolRoutes)
-app.use('/api/v1/users', userRoutes)
-app.use('/api/v1/groups', groupRoutes) */
-
-app.use('/api/v1/healthcheck', healthchecktHandler)
+app.use('/api/v1/healthcheck', healthChecktHandler)
+app.use('/api/v1/task', taskRoutes)
 
 
 export default app
